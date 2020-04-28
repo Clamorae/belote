80d8eaaddc38e9095fe09992853f7138fa627512
Paul <paul.caillier2002@gmail.com>
2020-04-28 15:47:08 +0200

    get corrected mothe fucker + ajout d'une fuite de mémoire dégeulasse

--- sourcecode/main.c
+++ sourcecode/main.c
@@ -6,7 +6,6 @@
 #include"menu.h"
 
 int main(){
-    menu();
     int **card=create(2,32),a=0;
     for (size_t i = 0; i < 32; i++){
         if ((i+a)%10==8){
@@ -14,9 +13,9 @@ int main(){
         }
         card[0][i]=i+a;
     }
-    
+
     printf("\n");
-    **card=randomize(card);
+    randomize(card);
      for (size_t i = 0; i < 2; i++){
         printf("\n");
         for (size_t j = 0; j < 32; j++){
@@ -24,6 +23,7 @@ int main(){
         }
     }
     highscore *HSarray = getHighScore();
+    menu();
     if (menu() == 2) {printHighScore(HSarray);}
     writeHighScore(HSarray);
     return 0;
