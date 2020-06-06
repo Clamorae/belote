9ff097ed3fc7b89cd5ba4e1ab28474f20496e4bb
Clamore <rotuman.lenoob@gmail.com>
2020-06-06 15:56:38 +0200

    MES BEBE SAVENT JOUER

--- sourcecode/IA.c
+++ sourcecode/IA.c
@@ -89,29 +89,28 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
 
 
     if (turn==0){//this part of the fonction will only be active if the IA is the first to play
-        for (int j = 0; j < 8; j++){
-            cardsOfRound[turn] = playableCards[j];
-            for(int k=((player-1)*8);k<(player*8);k++){
-                if (cards[0][k] == playableCards[j]){
+        cardsOfRound[turn] = playableCards[0];
+        for(int k=((player-1)*8);k<(player*8);k++){
+            if (cards[0][k] == cardsOfRound[turn]){
                 cards[1][k] = 0;
-                }
             }
-        j=8;
         }
     }
+    
 
-    else if(atoutMode==0 && atout==playableCards[0]/10){//In this case the IA doest have the color which is played by the leader sp Ia will play an atout
+    else if(atoutMode==0 && atout==playableCards[0]/10){//In this case the IA doest have the color which is played by the leader so IA will play an atout
         int value[8]={0,1,2,4,5,6,3,7};
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
                 if (playableCards[j]%10==value[i]){
                     cardsOfRound[turn] = playableCards[j];
                     for(int k=((player-1)*8);k<(player*8);k++){
-                        if (cards[0][k] == playableCards[j]){
+                        if (cards[0][k] == cardsOfRound[turn]){
                             cards[1][k] = 0;
                         }
                     }
                     j=8;
+                    i=8;
                 }
             }
         }
@@ -121,7 +120,7 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
     else if (atoutMode==0 && cardsOfRound[0]/10==playableCards[0]/10){
         int value[8]={0,1,2,4,5,6,3,7};
         for (int i = 0; i < turn; i++){
-            if (cardsOfRound[turn]/10==cardsOfRound[0]/10){
+            if (cardsOfRound[i]/10==cardsOfRound[0]/10){
                 for (int j = 0; j < 8; j++){
                     if (value[j]==cardsOfRound[turn]%10){
                         if (j>higher){
@@ -143,11 +142,12 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
                         }
                     }
                     j=8;
+                    i=8;
                 }
             }
         }
         if (winnable==false){
-            for (int i = higher; i > -1; i--){
+            for (int i = 0; i < higher; i++){
                 for (int j = 0; j < 8; j++){
                     if (playableCards[j]%10==value[i]){
                         cardsOfRound[turn] = playableCards[j];
@@ -157,6 +157,7 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
                             }
                         }
                         j=8;
+                        i=8;
                     }
                 }
             }
@@ -190,11 +191,12 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
                             }
                         }
                         j=8;
+                        i=8;
                     }
                 }
             }
             if (winnable==false){
-                for (int i = higher; i > -1; i--){
+                for (int i = 0; i < higher; i++){
                     for (int j = 0; j < 8; j++){
                         if (playableCards[j]%10==value[i]){
                             cardsOfRound[turn] = playableCards[j];
@@ -204,6 +206,7 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
                                 }
                             }
                             j=8;
+                            i=8;
                         }
                     }
                 }
@@ -221,6 +224,7 @@ void IAplayCard(int** cards, int* cardsOfRound, int atoutMode, int atout,int pla
                             }
                         }
                         j=8;
+                        i=8;
                     }
                 }
             }
@@ -300,53 +304,3 @@ void IAgetplayablecards(int** cards,int* cardsOfRound, int* playableCards, int a
         }
     }
 }
-
-/*int IAgetplayablecards(int** cards, int* cardsOfRound,int* playableCards, int atoutMode, int atout,int player){
-    int NofPCards = 0;
-    int colorToMatch;
-    if(atoutMode == 1){
-        colorToMatch = atout;
-    }
-    else{
-        colorToMatch = cardsOfRound[0]/10;
-    }
-
-    if(cardsOfRound[0] == -1){
-        IAcompareAndAdd(cards,playableCards,&NofPCards,-1,player);
-    }
-    else{
-        IAcompareAndAdd(cards,playableCards,&NofPCards,colorToMatch,player);
-        if (NofPCards == 0){
-            IAcompareAndAdd(cards,playableCards,&NofPCards,-1,player);
-            if(atoutMode != 1){
-                IAcompareAndAdd(cards,playableCards,&NofPCards,atout,player);
-                if (NofPCards == 0){
-                    IAcompareAndAdd(cards,playableCards,&NofPCards,-1,player);
-                }
-            }
-            else{
-                IAcompareAndAdd(cards,playableCards,&NofPCards,-1,player);
-            }
-        }
-    }
-    return NofPCards;
-}
-
-void IAcompareAndAdd(int** cards, int* playableCards,int* NofPCards, int colorToCompare,int player){
-    if(colorToCompare == -1){
-        for(int i=((player-1)*8);i<(player*8);i++){
-            if(cards[1][i] != 0){
-                playableCards[*NofPCards] = cards[0][i];
-                (*NofPCards)++;
-            }
-        }
-    }
-    else{
-        for(int i=((player-1)*8);i<(player*8);i++){
-            if(cards[1][i] != 0 && cards[0][i]/10 == colorToCompare){
-                playableCards[*NofPCards] = cards[0][i];
-                (*NofPCards)++;
-            }
-        }
-    }
-}*/
